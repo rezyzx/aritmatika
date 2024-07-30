de9f0e523d038c17c2a5faa1167fa37cbe384fef
USER <USER@DESKTOP-6H9CVHN>
2024-07-30 11:47:13 +0700

    Add project files.

---
+++ aritmatika/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aritmatika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama, asal;
+            Console.WriteLine("masukan nama");
+                nama = Console.ReadLine();
+            Console.WriteLine("masukan asal");
+            asal = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("hallo (8) dari (1) selamat datang di c#",nama,asal);
+        }
+    }
+}
---
+++ aritmatika2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aritmatika2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka1, angka2, hasil;
+            Console.WriteLine("silahkan masukan angka pertama");
+            angka1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("silahkan masukan angka kedua");
+            angka2 = int.Parse(Console.ReadLine());
+
+            hasil = angka1 + angka2;
+
+            Console.WriteLine(hasil);
+
+
+        }
+    }
+}
